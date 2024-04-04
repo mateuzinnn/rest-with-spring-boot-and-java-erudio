1aadcd26b96b50a2ce660ce06d0fe1a49994594d
Mateus Almeida <mateusva.1132@gmail.com>
2024-04-04 18:09:19 -0300

    feat: implementação método findAll()

--- FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio-1/src/main/java/br/com/erudio/controllers/PersonController.java
+++ FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio-1/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -1,5 +1,6 @@
 package br.com.erudio.controllers;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,12 @@ public class PersonController {
 	
 	@Autowired
 	private PersonService service;
+	
+	@RequestMapping(method = RequestMethod.GET,
+					produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}
 	
 	@RequestMapping(value = "/{id}",
 			method=RequestMethod.GET,
--- FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio-1/src/main/java/br/com/erudio/services/PersonService.java
+++ FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio-1/src/main/java/br/com/erudio/services/PersonService.java
@@ -1,5 +1,7 @@
 package br.com.erudio.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,18 @@ public class PersonService {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonService.class.getName());
 	
+	public List<Person> findAll() {
+		logger.info("finding all people...");
+		List<Person> persons = new ArrayList<>();
+		
+		for(int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+
 	public Person findById(String id) {
 		
 		logger.info("finding one person...");
@@ -26,4 +40,17 @@ public class PersonService {
 		
 		return person;
 	}
+	
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name: " + i);
+		person.setLastName("Last name: " + i);
+		person.setAddress("Some address in Brasil: " + i);
+		person.setGender("Male");
+		
+		return person;
+	}
+
 }
